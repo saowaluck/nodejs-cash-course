d0d5dbcac899e43cf9721830db135d2e162d5caf
saowalucksuksawat <ssuksawat@deem.com>
2024-10-01 23:00:25 +0700

    Read file index.html into res.write(getPage('index.html'))

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 //import {calculateVat} from './utils'
-
-
 const http = require('http')
+const path = require('path')
+const fs = require('fs')
+
+const getPage = (page) => {
+  return fs.readFileSync(path.join(__dirname, page))
+}
+
 http.createServer((req, res)=> {
   res.setHeader('Content-type', 'text/html')
   res.writeHead(200)
-  res.write('<h1>Hello</h1>')
+  res.write(getPage('index.html'))
   res.end()
 }).listen("5001")
 
