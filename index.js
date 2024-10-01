b6f8a413ffca43e0bad88f170794118726de483f
saowalucksuksawat <ssuksawat@deem.com>
2024-10-01 23:17:08 +0700

    Add route for detect page change

--- index.js
+++ index.js
@@ -7,11 +7,26 @@ const getPage = (page) => {
   return fs.readFileSync(path.join(__dirname, page))
 }
 
-http.createServer((req, res)=> {
-  res.setHeader('Content-type', 'text/html')
-  res.writeHead(200)
-  res.write(getPage('index.html'))
+http.createServer((req, res) => {
+  console.log("server staring on http://localhost:5001")
+  const fileType = path.extname(req.url) || ".html"
+
+  if (fileType === ".html") {
+    res.setHeader('Content-type', 'text/html')
+    res.writeHead(200)
+
+    if (req.url === "/") {
+      res.write(getPage('index.html'))
+    } else {
+      res.write(getPage(`${req.url}.html`))
+    }
+
+  } else {
+    res.writeHead(404)
+  }
   res.end()
+
+
 }).listen("5001")
 
 // Build in module
