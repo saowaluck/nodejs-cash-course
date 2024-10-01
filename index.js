6e07f280e257877d1dfeb60c596581683cceb861
saowalucksuksawat <ssuksawat@deem.com>
2024-10-01 22:23:20 +0700

    Using fs - file system wirteFile, readFile

--- index.js
+++ index.js
@@ -17,3 +17,22 @@ console.log(path.parse(__filename))
 console.log(path.join(__dirname, 'utils.js'))
 
 
+//File system
+const fs = require('fs')
+fs.writeFileSync(path.join(__dirname, 'data.txt'), "Helloi Sync")
+console.log("read file sync::::", fs.readFileSync(path.join(__dirname, 'data.txt'), "utf8"))
+
+
+fs.writeFile(path.join(__dirname, 'data.txt'), "Hello Async", () => {
+  console.log("finish writting file")
+  console.log("read file Async::::", fs.readFileSync(path.join(__dirname, 'data.txt'), "utf8"))
+
+})
+
+
+console.log(path.join(__dirname, 'data.txt'))
+// console.log(fs.readFileSync(path.join(__dirname, 'data.txt')))
+// console.log(fs.readFileSync(path.join(__dirname, 'data.txt'), 'utf8'))
+
+
+
