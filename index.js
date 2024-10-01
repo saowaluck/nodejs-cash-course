da0c867127d6fabf206f9d66d81afdb2706d8cbf
saowalucksuksawat <ssuksawat@deem.com>
2024-10-01 22:05:20 +0700

    Use buid-in *PATH* basename, dirname, extname, parse, join

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ console.log("vat is", vat)
 
 console.log(__filename)
 console.log(__dirname)
+
+
+// Build in module
+const path = require('path')
+console.log(path.basename(__filename))
+console.log(path.dirname(__filename))
+console.log(path.extname(__filename))
+console.log(path.parse(__filename))
+console.log(path.join(__dirname, 'utils.js'))
+
+
