d1fc0ce5b32136130ca097f38eae674c9fe2cb8e
saowalucksuksawat <ssuksawat@deem.com>
2024-10-01 22:30:51 +0700

    Add events it's use for trigger when have something using for trigger like a chantbot

--- index.js
+++ index.js
@@ -41,3 +41,13 @@ const os = require("os")
 console.log(os.cpus())
 console.log(os.homedir())
 console.log(os.uptime())
+
+//Events
+const events = require('events')
+const EventEmitter = events.EventEmitter
+const connect = new EventEmitter
+
+connect.on("online", () => {
+  console.log("event on")
+})
+connect.emit("online")
